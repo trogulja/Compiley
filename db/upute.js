0d5296180f7589db42a6adc47dee53bedbae444d
trogulja <tibor.rogulja@tiskarazagreb.hr>
2020-10-21 17:41:13 +0200

    test

---
+++ db/upute.js
@@ -0,0 +1,22 @@
+/**
+ * klijent
+ * desk
+ * proizvod
+ */
+
+const known = {};
+
+const klijent_administracija = {
+  edukacija: ['trening', 'workshop'],
+  odrzavanje: ['claro', 'dti', 'pamendo', 'kompjuteri', 'monitori', 'ciscenje'],
+  marketing: ['layout navlakuše', 'slike', 'mockupovi'],
+  sastanak: ['interni', 'eksterni'],
+  razvoj: ['web stranice', 'excel', 'aplikacije'],
+  austrija: ['treba definirati']
+}; // dodati "ništa od navedenog"
+
+const klijent_tizg = {
+  asura: ['pdf check -> soboške novine'],
+  layout: [],
+  slike: [],
+} // dodati "ostalo"
